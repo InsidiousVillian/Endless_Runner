4a689be9adabec83a96bcb6bea5cf135e0a182e1
Jet Kingston <162489467+InsidiousVillian@users.noreply.github.com>
2025-03-23 22:03:50 +0200

    Section Manager created
    This script contains all sections created in a list, when there is more than 2 sections alive at once the oldest one will be destroyed. However 1 bug, first section does not destory, this could be be because i made the section logic inside the OnEnterCollision which means the list only starts when it hits the first barrier meaning the first running section is not added to the list

--- EndlessRun/Assets/Scripts/Player_Scripts/SectionManager.cs
+++ EndlessRun/Assets/Scripts/Player_Scripts/SectionManager.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SectionManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Singleton instance
+    public static SectionManager Instance { get; private set; }
+    
+    // List to track all sections in order of creation
+    private List<GameObject> sectionList = new List<GameObject>();
+    
+    // Number of sections to keep (including current)
+    public int sectionsToKeep = 2;
+    
+    private void Awake()
     {
+        // Singleton pattern setup
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         
+        // Initialize the list
+        sectionList = new List<GameObject>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    // Add a section to the list when it's created
+    public void AddSection(GameObject section)
     {
+        sectionList.Add(section);
         
+        // If we have more sections than we want to keep
+        if (sectionList.Count > sectionsToKeep)
+        {
+            // Destroy the oldest section
+            GameObject oldestSection = sectionList[0];
+            sectionList.RemoveAt(0);
+            Destroy(oldestSection);
+        }
     }
 }
--- EndlessRun/Assets/Scripts/Player_Scripts/SectionTrigger.cs
+++ EndlessRun/Assets/Scripts/Player_Scripts/SectionTrigger.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
 public class SectionTrigger : MonoBehaviour
 {
     public GameObject runningSection;
     public float sectionLength = 30f; // Length of each section
+    public int sectionsToKeep = 2;
     private bool hasTrigger = false;
+   
+    //used to store sections in a list
+    private static List<GameObject> activeSections = new List<GameObject>();
     
     private void OnTriggerEnter(Collider other)
     {
@@ -11,18 +18,24 @@ public class SectionTrigger : MonoBehaviour
         {
             // Finds the forward position where the next section should spawn
             Vector3 spawnPosition = transform.position + Vector3.forward * sectionLength;
-            
+           
             // Keeps the same X and y positions
             spawnPosition.x = 1;
             spawnPosition.y = 0;
-            
+           
             // Instantiate the new section at the calculated position
-            Instantiate(runningSection, spawnPosition, Quaternion.identity);
-            
+            GameObject newSection = Instantiate(runningSection, spawnPosition, Quaternion.identity);
+            activeSections.Add(newSection);
+            if(activeSections.Count > sectionsToKeep){
+                GameObject oldestSection = activeSections[0];
+                activeSections.RemoveAt(0);
+                Destroy(oldestSection);
+            }
+           
             hasTrigger = true;
         }
     }
-    
+   
     private void OnTriggerExit(Collider other)
     {
         // Resets the trigger when the other object exits
@@ -31,9 +44,22 @@ public class SectionTrigger : MonoBehaviour
             ResetTrigger();
         }
     }
-    
+   
     public void ResetTrigger()
     {
         hasTrigger = false;
+    }
+    
+    /*destroys all active sections and clears tracking list*/
+    public static void ClearAllSections(){
+        //goes through each section stored in list
+        foreach(GameObject section in activeSections){
+            //checks if section ref is still valid before destorying - prevents errors
+            if(section != null){
+                //destroys section
+                Destroy(section);
+            }
+        }
+        activeSections.Clear();
     }
 }
