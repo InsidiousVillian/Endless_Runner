9e96b6aa65bcf25303c8b30a7d2166e14e330e6c
Jet Kingston <162489467+InsidiousVillian@users.noreply.github.com>
2025-03-23 19:04:36 +0200

    creates one new running section but no other

--- EndlessRun/Assets/Scripts/Player_Scripts/SectionTrigger.cs
+++ EndlessRun/Assets/Scripts/Player_Scripts/SectionTrigger.cs
@@ -4,14 +4,18 @@ public class SectionTrigger : MonoBehaviour
 {
     public GameObject runningSection;
     private bool hasTrigger = false;
-
-    private void OnTriggerEnter(Collider other){
-        if (other.gameObject.CompareTag("trigger") && !hasTrigger){
-            Instantiate(runningSection);
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("trigger") && !hasTrigger)
+        {
+            Instantiate(runningSection, new Vector3(1, 0, 28), Quaternion.identity);
             hasTrigger = true;
         }
     }
-    public void ResetTrigger(){
+    
+    public void ResetTrigger()
+    {
         hasTrigger = false;
     }
 }
