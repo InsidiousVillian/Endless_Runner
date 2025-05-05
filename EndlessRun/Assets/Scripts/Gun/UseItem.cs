5b93b4baeb13e978bbd10529772f8b269c11a448
Jet Kingston <jettaikingston10@gmail.com>
2025-05-05 21:54:56 +0200

    Gun works, bullet works
    guns can now be used to destroy obstacles. Will create random spawning of the guns soon.

---
+++ EndlessRun/Assets/Scripts/Gun/UseItem.cs
@@ -0,0 +1,33 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class UseItem : MonoBehaviour
+{
+    public GameObject itemToUse;
+    public GameObject Bullet;
+    public GameObject bulletSpawner;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)){
+            UseGun();
+        }
+    }
+
+    void UseGun(){
+        if (ItemPickup.guns.Count > 0){
+            //grabbing the first gun in the list
+            itemToUse = ItemPickup.guns[0];
+            Debug.Log("using gun");
+            //spawns bullet
+            Instantiate(Bullet, bulletSpawner.transform.position, quaternion.identity);
+
+            //removes gun from list 
+            ItemPickup.guns.RemoveAt(0);
+        }
+        else{
+            Debug.Log("item is not beign used");
+        }
+    }
+}
