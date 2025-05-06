b2a6624a253aa6768a6ba8aafb07d9bdbefae4ca
Jet Kingston <jettaikingston10@gmail.com>
2025-05-06 10:45:51 +0200

    Updated MasterInfo and UI
    Gun count added to UI so player can identify how many guns he has.

--- EndlessRun/Assets/Scripts/UI/MasterInfo.cs
+++ EndlessRun/Assets/Scripts/UI/MasterInfo.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class MasterInfo : MonoBehaviour
 {
     [SerializeField] GameObject bottleDisplay;
+    [SerializeField] GameObject gunDisplay;
     public static int bottleCount = 0;
     private void Start()
     {
         Debug.LogError(this.name   + "HELL"     );
         bottleCount = 0;
+        
     }
     // Update is called once per frame
     void Update()
@@ -15,6 +17,8 @@ public class MasterInfo : MonoBehaviour
         if (bottleCount >= 0) 
         {
             bottleDisplay.GetComponent<TMPro.TMP_Text>().text = "Bottles:" + bottleCount;
+            gunDisplay.GetComponent<TMPro.TMP_Text>().text = "Guns:" + ItemPickup.guns.Count;
+            
         }
         
     }
