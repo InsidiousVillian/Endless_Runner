01514dfd208aadf1f6d689e4ecb41124f4ef460e
Jet Kingston <jettaikingston10@gmail.com>
2025-05-07 13:35:37 +0200

    MasterInfo updated
    Gun count now updates

--- EndlessRun/Assets/Scripts/UI/MasterInfo.cs
+++ EndlessRun/Assets/Scripts/UI/MasterInfo.cs
@@ -5,12 +5,15 @@ public class MasterInfo : MonoBehaviour
     [SerializeField] GameObject bottleDisplay;
     [SerializeField] GameObject gunDisplay;
     public static int bottleCount = 0;
-    public static int Count = 0;
     private void Start()
     {
         Debug.LogError(this.name   + "HELL"     );
         bottleCount = 0;
-        Count = 0;
+        
+        //checking if list has be initialised
+        if(ItemPickup.guns != null){
+            ItemPickup.guns.Clear();
+        }
     }
     // Update is called once per frame
     void Update()
