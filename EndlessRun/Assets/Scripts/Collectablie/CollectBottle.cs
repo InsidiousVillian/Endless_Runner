73b61554e3a399f2ac4f6fe8e451031a73021977
Jet Kingston <jettaikingston10@gmail.com>
2025-05-10 20:25:02 +0200

    updated

--- EndlessRun/Assets/Scripts/Collectablie/CollectBottle.cs
+++ EndlessRun/Assets/Scripts/Collectablie/CollectBottle.cs
@@ -4,12 +4,10 @@ using UnityEngine.SceneManagement;
 public class CollectBottle : MonoBehaviour
 {
     void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) {
+        if  (other.CompareTag("Player")) {
             Debug.Log("Bottles is collected. Current count : " + (MasterInfo.bottleCount + 1));
             MasterInfo.bottleCount += 1;
             Destroy(gameObject);
         }
-
-    }
-    
+    }   
 }
