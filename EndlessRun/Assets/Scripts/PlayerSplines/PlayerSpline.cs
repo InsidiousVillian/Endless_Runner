c2089bfb12151a24583edfd98a9a2712821b3e2f
JKS42 <133946170+JKS42@users.noreply.github.com>
2025-05-14 16:57:34 +0200

    Adding and testing out scripts for the player in the boss level

--- EndlessRun/Assets/Scripts/PlayerSplines/PlayerSpline.cs
+++ EndlessRun/Assets/Scripts/PlayerSplines/PlayerSpline.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class PlayerSpline : MonoBehaviour
 {
+    public SplineContainer splineContainer;
+    public float speed = 1f;
+    private float t = 0f;
+    private bool followSpline = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (followSpline)
+        {
+            t += Time.deltaTime * speed;
+            t = Mathf.Clamp01(t);
+            transform.position = splineContainer.Spline.EvaluatePosition(t);
+            Vector3 direction = splineContainer.Spline.EvaluateTangent(t);
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("AttachTrigger"))
+        {
+            followSpline = true;
+        }
+        if (other.CompareTag("DetachTrigger")) // Check if the collider is the detach trigger
+        {
+            followSpline = false; // Stop following the spline
+        }
     }
 }
+
+
