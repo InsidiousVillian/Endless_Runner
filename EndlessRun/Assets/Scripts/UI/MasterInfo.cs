05f8ad1e34e9c61b24ce631b0f735e9c69206530
Jet Kingston <jettaikingston10@gmail.com>
2025-05-17 15:26:21 +0200

    Clock added

--- EndlessRun/Assets/Scripts/UI/MasterInfo.cs
+++ EndlessRun/Assets/Scripts/UI/MasterInfo.cs
@@ -7,16 +7,19 @@ public class MasterInfo : MonoBehaviour
     [SerializeField] GameObject highNoonDisplay;
     public static int bottleCount = 0;
 
+
     private void Start()
     {
-        Debug.LogError(this.name   + "HELL"     );
+        Debug.LogError(this.name + "HELL");
         bottleCount = 0;
-        
+
         //checking if list has be initialised
-        if(ItemPickup.guns != null){
+        if (ItemPickup.guns != null)
+        {
             ItemPickup.guns.Clear();
         }
-        if(HighNoonPickup.highNoonList != null){
+        if (HighNoonPickup.highNoonList != null)
+        {
             HighNoonPickup.highNoonList.Clear();
         }
     }
@@ -25,19 +28,11 @@ public class MasterInfo : MonoBehaviour
     {
         if (bottleCount >= 0) 
         {
-
-            if(bottleDisplay != null){
-                //displays bottle count 
-                bottleDisplay.GetComponent<TMPro.TMP_Text>().text = "Bottles:" + bottleCount;//display guns using itemPickup list
-            }
-            if(gunDisplay != null && ItemPickup.guns != null){
-                gunDisplay.GetComponent<TMPro.TMP_Text>().text = "Guns:" + ItemPickup.guns.Count;
-            }
-
-            if(highNoonDisplay != null  && HighNoonPickup.highNoonList != null){
-               //displays High noon using list
-                highNoonDisplay.GetComponent<TMPro.TMP_Text>().text = "High Noon:" + HighNoonPickup.highNoonList.Count; 
-            }   
+            bottleDisplay.GetComponent<TMPro.TMP_Text>().text = "Bottles:" + bottleCount;
+           
+            gunDisplay.GetComponent<TMPro.TMP_Text>().text = "Guns:" + ItemPickup.guns.Count;
+           
+            highNoonDisplay.GetComponent<TMPro.TMP_Text>().text = "High Noon:" + HighNoonPickup.highNoonList.Count;  
         }      
     }
 }
