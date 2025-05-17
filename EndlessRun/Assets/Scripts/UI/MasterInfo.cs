e8f8aaeac7f3a226d671debe1e9b649f38ae8231
Jet Kingston <jettaikingston10@gmail.com>
2025-05-17 16:22:50 +0200

    Fixed Bug
    counter continue to count and player can reestart multiple times/

--- EndlessRun/Assets/Scripts/UI/MasterInfo.cs
+++ EndlessRun/Assets/Scripts/UI/MasterInfo.cs
@@ -8,12 +8,37 @@ public class MasterInfo : MonoBehaviour
     public static int bottleCount = 0;
 
 
+
+
     private void Start()
     {
-        Debug.LogError(this.name + "HELL");
+        Debug.LogError(this.name + " HELL");
+
+
+        if (bottleDisplay == null)
+        {
+             bottleDisplay = GameObject.Find("BottleCount");
+            
+        }
+        if (gunDisplay == null)
+        {
+            gunDisplay = GameObject.Find("gunCount");
+            
+        }
+        if (highNoonDisplay == null)
+        {
+            highNoonDisplay = GameObject.Find("HighNoonCount");
+            
+        }
+
+
+        if (bottleDisplay == null || gunDisplay == null || highNoonDisplay == null)
+        {
+            Debug.LogWarning("One or more UI elements are missing in the scene.");
+        }
+
         bottleCount = 0;
 
-        //checking if list has be initialised
         if (ItemPickup.guns != null)
         {
             ItemPickup.guns.Clear();
@@ -26,13 +51,11 @@ public class MasterInfo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (bottleCount >= 0) 
+         if (bottleDisplay && gunDisplay && highNoonDisplay)
         {
-            bottleDisplay.GetComponent<TMPro.TMP_Text>().text = "Bottles:" + bottleCount;
-           
-            gunDisplay.GetComponent<TMPro.TMP_Text>().text = "Guns:" + ItemPickup.guns.Count;
-           
-            highNoonDisplay.GetComponent<TMPro.TMP_Text>().text = "High Noon:" + HighNoonPickup.highNoonList.Count;  
-        }      
+            bottleDisplay.GetComponent<TMPro.TMP_Text>().text = "Bottles: " + bottleCount;
+            gunDisplay.GetComponent<TMPro.TMP_Text>().text = "Guns: " + ItemPickup.guns.Count;
+            highNoonDisplay.GetComponent<TMPro.TMP_Text>().text = "High Noon: " + HighNoonPickup.highNoonList.Count;
+        }
     }
 }
