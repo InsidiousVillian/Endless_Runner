e2f4c0e2d9fed31869490fdff3e354e8cf7c4130
JKS42 <kian.jsmit@gmail.com>
2025-05-19 21:31:20 +0200

    Boss Spawning and lighting

--- EndlessRun/Assets/Scripts/BossMechanics/EnemyAI.cs
+++ EndlessRun/Assets/Scripts/BossMechanics/EnemyAI.cs
@@ -1,33 +1,52 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class EnemyAI : MonoBehaviour
 {
+    public GameObject cowBoy;
     public Transform player;
+    public Transform Spwn;
     public float moveSpeed = 3f;
     public float timer = 0f;
+    public float spawnerTime = 0f;
     public float delay = 3f;
+    public float spawnDelay = 7f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         transform.LookAt(PlayerMovement.instance.transform);
+        //spawnerTime += Time.deltaTime;
+        //if (spawnerTime >= spawnDelay)
+        //{
+        //    cowBoy.SetActive(true);
+        //}
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        
+    }
     void FixedUpdate()
     {
-        if (player == null) return;
 
+        if (player == null) return;
         timer += Time.fixedDeltaTime;
-        if(timer >= delay)
+        if (timer >= delay)
         {
             // Move towards the player
+            
             transform.position = Vector3.MoveTowards(
-                transform.position,
-                player.position,
-                moveSpeed * Time.deltaTime
+                    transform.position,
+                    player.position,
+                    moveSpeed * Time.deltaTime
             );
+
             
         }
         
+        
     }
+    
 }
