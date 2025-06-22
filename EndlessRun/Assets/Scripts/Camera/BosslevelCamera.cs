a19f754470ba3af1dc8a6744ec5f86cc4830239c
JKS42 <kian.jsmit@gmail.com>
2025-06-22 20:19:21 +0200

    BossLevel2

--- EndlessRun/Assets/Scripts/Camera/BosslevelCamera.cs
+++ EndlessRun/Assets/Scripts/Camera/BosslevelCamera.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 public class BosslevelCamera : MonoBehaviour
 {
     [SerializeField] private GameObject Villian;
+    public Transform target;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.LookAt(EnemyAI.Instance.transform);
+        transform.LookAt(target);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(target);
     }
 }
